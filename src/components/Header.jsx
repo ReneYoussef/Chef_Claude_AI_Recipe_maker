8968c9bd36508a3b437a47b2cd63a9eef26cce95
Rene A Youssef <Reneabdo10@gmail.com>
2025-05-08 15:16:26 +0300

    Chef Claude web app

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+
+    return(
+        <header className="header">
+            <img src="src/assets/chef.png" alt="chef-icon" className="header-icon" />
+            <span className="header-title">Chef Claude</span>
+        </header>
+    )
+}
